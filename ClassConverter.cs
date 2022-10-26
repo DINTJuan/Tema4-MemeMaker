4d75276d32ae9112e8dfa692192d16b56b5961ad
alumno <alumno@A302PC26>
2022-10-26 12:50:16 +0200

    Agregar archivos de proyecto.

---
+++ ClassConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Tema4_MemeMaker
+{
+    class ClassConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool estado = (bool)value;
+            if(estado)
+            {
+                return new Thickness(2);
+            }
+            else
+            {
+                return new Thickness(0);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
